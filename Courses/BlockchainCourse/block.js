3f736e9e6f8c2ecb641349b206a4f5ce75ac654d
Anders Bille <anders.bille.lind@gmail.com>
2021-02-07 11:09:11 +0100

    start of TDD of block.js in block.test.js

--- Courses/BlockchainCourse/block.js
+++ Courses/BlockchainCourse/block.js
@@ -1,15 +1,11 @@
-class Block {
-    constructor(timestamp, lastHash, hash, data) {
-        this.timestamp = timestamp;
-        this.lastHash = lastHash
-        this.hash = hash;
-        this.data = data;
-
-    }
+class Block { // ved at wrappe parametrene i {} gør det så man ikke senere skal indføre det i denne rækkefølge, Map funktionen gør dette muligt
+    // constructor({timestamp, lastHash, hash, data}) {
+    //     this.timestamp = timestamp;
+    //     this.lastHash = lastHash
+    //     this.hash = hash;
+    //     this.data = data;
+    // }
 }
 
 
-
-class BlockChain() {
-    
-}
+module.exports = Block;
