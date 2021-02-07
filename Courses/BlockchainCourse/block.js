dd0940e2922d2de0c64a59e8dcfa57e5b4cd6644
Anders Bille <anders.bille.lind@gmail.com>
2021-02-07 11:28:43 +0100

    first TDD succes

--- Courses/BlockchainCourse/block.js
+++ Courses/BlockchainCourse/block.js
@@ -1,10 +1,10 @@
 class Block { // ved at wrappe parametrene i {} gør det så man ikke senere skal indføre det i denne rækkefølge, Map funktionen gør dette muligt
-    // constructor({timestamp, lastHash, hash, data}) {
-    //     this.timestamp = timestamp;
-    //     this.lastHash = lastHash
-    //     this.hash = hash;
-    //     this.data = data;
-    // }
+    constructor({timestamp, lastHash, hash, data}) {
+        this.timestamp = timestamp;
+        this.lastHash = lastHash
+        this.hash = hash;
+        this.data = data;
+    }
 }
 
 
--- Courses/BlockchainCourse/block.test.js
+++ Courses/BlockchainCourse/block.test.js
@@ -1,3 +1,22 @@
-const { describe } = require("yargs");
+const Block = require("./block");
 
-describe('Block', )
+describe('Block', () => {
+    const timestamp = 'a-date';
+    const lastHash = 'foo-lastHash';
+    const data = ['blockchain', 'data'];
+    const hash = 'foo-hash';
+    // const block = new Block({
+    //     timestamp: timestamp,
+    //     lastHash: lastHash,
+    //     data: data,
+    //     hash: hash,
+    // }); dette er muligt da datatypen er den samme, så man kun skal skrive det ind en gang.
+    const block = new Block({timestamp, lastHash, data, hash})
+
+    it('has a timestamp, lastHash, data, hash property', () =>{
+        expect(block.timestamp).toEqual(timestamp);
+        expect(block.lastHash).toEqual(lastHash);
+        expect(block.hash).toEqual(hash);
+        expect(block.data).toEqual(data);
+    });
+});
