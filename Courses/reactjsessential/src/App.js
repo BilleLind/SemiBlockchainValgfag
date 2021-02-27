b142115c9e361333418b8425965f78ea2d93b373
Anders Bille <anders.bille.lind@gmail.com>
2021-02-27 21:29:12 +0100

    useEffect intro

--- Courses/reactjsessential/src/App.js
+++ Courses/reactjsessential/src/App.js
@@ -1,17 +1,27 @@
 import './App.css';
 import subject from './'
 import sample from './sample.jpg'
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 
 
 function App() {
   const [emotion, setEmotion] = useState("happy");
+  const [secondary, setSecondary] = useState('tired');
+
+  useEffect(() => {
+    console.log(`it's ${emotion} around here!`);
+  }, [emotion]);
+
+
+  useEffect(()=> {
+    console.log(`it's ${secondary} around here `);
+  }, [secondary]);
 
   return(
   <>
       <h1>Hello state is {emotion}</h1>
       <button onClick={() => setEmotion("angry")}>angry</button>
-      <button onClick={() => setEmotion("happy")}>Happy</button> 
+      <button onClick={() => setSecondary("happy")}>Happy</button> 
   </>
   );
 }
