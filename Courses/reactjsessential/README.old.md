2daaea6ba5be06da73614e941192733192e53ae2
Anders Bille <anders.bille.lind@gmail.com>
2021-02-27 21:44:47 +0100

    useReducer

--- Courses/reactjsessential/README.old.md
+++ Courses/reactjsessential/README.old.md
@@ -6,3 +6,17 @@ notes:
 - a Library, created back in 2013 by facebook
 
 
+array destructoring
+....
+const array = {"hey", "too"};
+console.log(array)  => 
+(2) ["hey", "too"]
+0: "hey"
+1: "too"
+length: 2
+__proto__: Array(0)
+
+instead i could =>
+const [one, two] = {"one", "two"};
+console.log(one) => one
+console.log(two) => two
--- Courses/reactjsessential/src/App.js
+++ Courses/reactjsessential/src/App.js
@@ -1,27 +1,21 @@
 import './App.css';
 import subject from './'
 import sample from './sample.jpg'
-import { useState, useEffect } from 'react';
-
+import { useReducer,  } from 'react';
 
 function App() {
-  const [emotion, setEmotion] = useState("happy");
-  const [secondary, setSecondary] = useState('tired');
+  const [checked, toogle] = useReducer(
+    (checked => !checked),
+    false
+  );
 
-  useEffect(() => {
-    console.log(`it's ${emotion} around here!`);
-  }, [emotion]);
-
-
-  useEffect(()=> {
-    console.log(`it's ${secondary} around here `);
-  }, [secondary]);
+  
 
   return(
   <>
-      <h1>Hello state is {emotion}</h1>
-      <button onClick={() => setEmotion("angry")}>angry</button>
-      <button onClick={() => setSecondary("happy")}>Happy</button> 
+    <input type="checkbox" 
+    value={checked} onChange={() => toogle()}/>  
+     <p>{checked ? "checked" : "not checked"}</p>
   </>
   );
 }
