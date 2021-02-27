77ab3c750344ea3bafa09c4e532570d98973a457
Anders Bille <anders.bille.lind@gmail.com>
2021-02-27 22:09:15 +0100

    preliminary error, loading and null with useEffect and UseState

--- Courses/reactjsessential/src/App.js
+++ Courses/reactjsessential/src/App.js
@@ -7,13 +7,24 @@ import { useState, useEffect  } from 'react';
 
 function App({login}) {
   const [data, setDate] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
 
   useEffect(() => {
+    if(!login) return;
+    setLoading(true);
     fetch(`https://api.github.com/users/${login}`)
     .then((response) => response.json())
-    .then(setDate);
-  }, [])
+    .then(setDate)
+    .then(() => setLoading(false))
+    .catch(setError);
+  }, [login]);
   
+  if(loading) return <h1>Loading...</h1>;
+
+  if(error) return <pre>{JSON.stringify(error, null, 2)}</pre>;
+
+  if(!data) return null;
 
   if(data) {
     return (<div>
@@ -22,7 +33,6 @@ function App({login}) {
       <img alt={data.login} src={data.avatar_url}/>
     </div>);
   }
-  return <div>No User Available</div>
 }
 
 export default App;
