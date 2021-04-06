1314086cb689f9126f70174b3837b97afafcb4f0
Anders Bille <anders.bille.lind@gmail.com>
2021-04-06 12:37:29 +0200

    redis added to script

--- Courses/BlockchainCourse/pubsub.js
+++ Courses/BlockchainCourse/pubsub.js
@@ -24,4 +24,6 @@ class PubSub {
 
 const testPubSub = new PubSub();
 
-testPubSub.publisher.publish(CHANNELS.TEST, 'foo');
+setTimeout(() => {
+    testPubSub.publisher.publish(CHANNELS.TEST, 'foo');
+}, 1000); 
