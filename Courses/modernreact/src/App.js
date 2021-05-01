00cb7830f2bbfec19c225586278c59429790cd6b
Anders Bille <anders.bille.lind@gmail.com>
2021-05-01 09:09:53 +0200

    runnable with old versions

--- Courses/modernreact/src/App.js
+++ Courses/modernreact/src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import './App.css';
+
 const App = () => (
-        <h1>
-            Hello World
-        </h1>
-    );
+    <div className="App">
+        <h1>Hello, there!</h1>
+    </div>
+);
 
 
 export default App;
--- Courses/modernreact/webpack.config.js
+++ Courses/modernreact/webpack.config.js
@@ -1,33 +1,37 @@
-const path = require("path");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
+const path = require('path');
+const webpack = require('webpack');
+
 
 module.exports = {
-    entry: path.join(__dirname, "src", "index.js"),
-    output: { path: path.join(__dirname, "build"), filename: "index.bundle.js" },
-    mode: process.env.NODE_ENV || "development",
-    resolve: { modules: [path.resolve(__dirname, "src"), "node_modules"] },
-    devServer: { contentBase: path.join(__dirname, "src") },
+    entry: './src/index.js',
+    mode: 'development',
     module: {
         rules: [
-            { 
-                test: /\.(js|jsx)$/, 
-                exclude: /node_modules/, 
-                use: ["babel-loader"] 
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /(node_modules)/,
+                loader: 'babel-loader',
+                options: {presets: ["@babel/env"]}
             },
             {
-                test: /\.(css|scss)$/,
-                use: ["style-loader", "css-loader"],
-            },
-            { 
-                test: /\.(jpg|jpeg|png|gif|mp3|svg)$/,
-                use: ["file-loader"] 
-            },
-        ],
+                test: /\.css$/,
+                use: ["style-loader", "css-loader"]
+            }
+        ]
+    },
+    resolve: {extensions: ['*', '.js', '.jsx']},
+    output: {
+        path: path.resolve(__dirname, 'dist/'),
+        publicPath: '/dist/',
+        filename: 'bundle.js'
+    },
+    devServer: {
+        contentBase: path.join(__dirname, 'public/'),
+        port: 3000,
+        publicPath: 'http//localhost:3000/dist/',
+        hotOnly: true
     },
     plugins: [
-        new HtmlWebpackPlugin({
-            template: path.join(__dirname, "src", "index.html"),
-        }),
-        
-    ],
+        new webpack.HotModuleReplacementPlugin(),
+    ]
 };
