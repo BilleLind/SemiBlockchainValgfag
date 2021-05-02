917b4cb21c50b9b4aae54727982879f984c11502
Anders Bille <anders.bille.lind@gmail.com>
2021-05-02 08:48:48 +0200

    connected componets to store

--- Courses/modernreact/readme.md
+++ Courses/modernreact/readme.md
@@ -7,6 +7,13 @@ https://www.linkedin.com/learning/building-modern-projects-with-react/installing
 
 -reselect => abstracting the states structure , how data is stored
 
+
+why redux =>
+prevent props drilling =>
+    - when you pass props through alot of different components
+    -f.eks. <App> => <List> => <ListItem> => <ETC>
+    
+
 ---------
 i found another way to do the 02_05, since it was outdated
 https://dev.to/deadwing7x/setup-a-react-app-with-webpack-and-babel-4o3k
--- Courses/modernreact/src/components/NewTodoForm.js
+++ Courses/modernreact/src/components/NewTodoForm.js
@@ -1,8 +1,11 @@
 import React, {useState} from 'react';
+import { from } from 'webpack-sources/lib/CompatSource';
+import { connect } from 'react-redux';
+import { createTodo } from './actions';
 
-
-const NewTodoForm = () => {
+const NewTodoForm = ({ todos, onCreatePressed }) => {
     const [inputValue, setInputValue] = useState("");
+
     return (
         <div className="new-todo-form">
             <input className="new-todo-input" 
@@ -10,10 +13,27 @@ const NewTodoForm = () => {
             placeholder="Type your new todo"
             value={inputValue}
             onChange={e=>setInputValue(e.target.value)}/>
-            <button className="new-todo-button">Create todo</button>
+            <button 
+            onClick={() =>{
+                const isDublicateText = todos.some(todo =>todo.text === inputValue);
+                if(!isDublicateText){
+                    onCreatePressed(inputValue);
+                    setInputValue('');
+                }
+            }}
+            className="new-todo-button">
+                Create todo
+            </button>
         </div>
     );
 }
 
+const mapStateToProps = state =>({
+    todos: state.todos,
+});
 
-export default NewTodoForm;
+const mapDispatchToProps = dispatch => ({
+    onCreatePressed: text => dispatch(createTodo(text)),
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(NewTodoForm);
