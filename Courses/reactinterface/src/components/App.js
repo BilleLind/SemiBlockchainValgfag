c0642218338f846dd2cae12a3dd1dee7a92a0acf
Anders Bille <anders.bille.lind@gmail.com>
2021-05-03 22:00:01 +0200

    components

---
+++ Courses/reactinterface/src/components/App.js
@@ -0,0 +1,33 @@
+import React, { Component } from 'react';
+import AddAppointments from './AddAppointments';
+import ListAppointments from './ListAppointments';
+import SearchAppointments from './SearchAppointments';
+import '../styles/App.css';
+
+
+class App extends Component {
+
+  constructor() {
+    super();
+  }
+
+  render() {
+    return (
+      <main className="page bg-white" id="petratings">
+      <div className="container">
+        <div className="row">
+          <div className="col-md-12 bg-white">
+            <div className="container">
+              <AddAppointments />
+              <SearchAppointments />
+              <ListAppointments />
+            </div>
+          </div>
+        </div>
+      </div>
+    </main>
+    );
+  }
+}
+
+export default App;
